82749629eb606702535de22bd2faead90ea83fe6
KodaliDhiya <112166720+KodaliDhiya@users.noreply.github.com>
2022-11-16 19:35:48 +0530

    Batch update

---
+++ T4H/js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1432},"pages":[{"id":"page-index","displayName":"index","link":{"linkType":"LinkTypePage","href":"#!page-index"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-ecandidate","displayName":"eCandidate","link":{"linkType":"LinkTypePage","href":"#!page-ecandidate"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
